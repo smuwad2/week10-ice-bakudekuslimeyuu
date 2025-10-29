277aeade05b7f2b1b1e8982fdf0a660739c81465
meiyu <tanmeiyuu@gmail.com>
2025-10-29 01:14:37 +0000

    comitto

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,12 +1,16 @@
 <script>
     //import component
+    import SoundIcon from './subcomponents/SoundIcon.vue'
     
     export default { 
+        components: {
+            SoundIcon, 
+        },
         data() {
             return {
                 soundLevel: 0
             }
-        }
+        }, 
     }
 </script>
 
@@ -15,7 +19,9 @@
    Sound level
     <input type="number" min="0" max="3" v-model="soundLevel"> 
     <!-- TODO: Add Code Here to make use of the component -->
-    
+    <SoundIcon v-bind:soundLevel ="soundLevel"></SoundIcon>
+    <!-- v-bind sound level is refeering   -->
+    <!-- "soundLevel" is referring to the parent  -->
 
 </template>
 
